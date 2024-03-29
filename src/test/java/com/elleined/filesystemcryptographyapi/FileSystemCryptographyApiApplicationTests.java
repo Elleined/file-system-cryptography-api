64371c39ba92598e51581a9cf674bf7ca33e53f1
Elleined <demadegu@gmail.com>
2024-03-29 20:41:09 +0800

    Refactored decryptor and decryptor services and implementations

--- src/test/java/com/elleined/filesystemcryptographyapi/FileSystemCryptographyApiApplicationTests.java
+++ src/test/java/com/elleined/filesystemcryptographyapi/FileSystemCryptographyApiApplicationTests.java
@@ -1,13 +1,18 @@
 package com.elleined.filesystemcryptographyapi;
 
+import com.elleined.filesystemcryptographyapi.encryptor.EncryptorService;
+import com.elleined.filesystemcryptographyapi.encryptor.EncryptorServiceImpl;
+import com.elleined.filesystemcryptographyapi.util.AESUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class FileSystemCryptographyApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
--- src/test/java/com/elleined/filesystemcryptographyapi/decryptor/DecryptorServiceImplTest.java
+++ src/test/java/com/elleined/filesystemcryptographyapi/decryptor/DecryptorServiceImplTest.java
@@ -1,25 +1,35 @@
 package com.elleined.filesystemcryptographyapi.decryptor;
 
+import com.elleined.filesystemcryptographyapi.encryptor.EncryptorService;
+import com.elleined.filesystemcryptographyapi.encryptor.EncryptorServiceImpl;
 import com.elleined.filesystemcryptographyapi.util.AESUtil;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.IvParameterSpec;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @ExtendWith(MockitoExtension.class)
 class DecryptorServiceImplTest {
 
     @Test
-    void decrypt() throws NoSuchAlgorithmException {
+    void decrypt() throws NoSuchAlgorithmException, InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException {
         // Expected Value
+        EncryptorService encryptorService = new EncryptorServiceImpl();
         DecryptorService decryptorService = new DecryptorServiceImpl();
 
         // Mock data
+        String data = "Hello World!";
         SecretKey secretKey = AESUtil.generateKey(128);
         IvParameterSpec iv = AESUtil.generateIv();
 
@@ -28,12 +38,16 @@ class DecryptorServiceImplTest {
         // Stubbing methods
 
         // Calling the method
-        // String originalValue = decryptorService.decrypt(decryptorService, iv, );
+        String encryptedText = encryptorService.encrypt(secretKey, iv, data);
+        String originalValue = decryptorService.decrypt(secretKey, iv, encryptedText);
 
         // Behavior Verifications
 
         // Assertions
-//        assertNotNull(originalValue);
+        assertNotNull(encryptedText);
+
+        assertNotNull(originalValue);
+        assertEquals(data, originalValue);
     }
 
     @Test
